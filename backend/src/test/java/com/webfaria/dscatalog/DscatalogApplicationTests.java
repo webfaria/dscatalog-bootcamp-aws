3700bffadb89be776a5b4241e4a203d79dc05127
Cleber Faria <webfaria@gmail.com>
2021-05-11 20:10:08 -0300

    Project created

---
+++ backend/src/test/java/com/webfaria/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfaria.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
