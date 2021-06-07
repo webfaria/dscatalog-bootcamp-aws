d97e05e2d45ea5028bc50e7be2eb5c3cc221e84d
Cleber <webfaria@gmail.com>
2021-06-07 06:16:54 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/webfaria/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/webfaria/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.webfaria.dscatalog.dto;
 
+import com.webfaria.dscatalog.services.validator.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
--- backend/src/main/java/com/webfaria/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/webfaria/dscatalog/repositories/UserRepository.java
@@ -8,4 +8,5 @@ import com.webfaria.dscatalog.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	User findByEmail(String email);
 }
