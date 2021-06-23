a451a07b6e049914d5e2bba39afe734f0b1ced28
Cleber <webfaria@gmail.com>
2021-06-22 22:42:30 -0300

    Search Products and Categories, Fix: Postgres compatibility

--- backend/src/main/java/com/webfaria/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/webfaria/dscatalog/repositories/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.webfaria.dscatalog.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.webfaria.dscatalog.entities.Category;
 import com.webfaria.dscatalog.entities.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+			+ "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
+	Page<Product> find(List<Category> categories, String name, Pageable pageable);
 
 }
