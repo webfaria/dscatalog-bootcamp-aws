d7076da6a473cb1926226a31808cec7f87744ff2
Cleber <webfaria@gmail.com>
2021-07-22 20:07:07 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,7 +1,14 @@
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
-function App() {
-  return <h1>Hello DSCatalog</h1>;
-}
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>;
+    </>
+  );
+};
 
 export default App;
